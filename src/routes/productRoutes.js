3b11247cf33dbd734462f4fdaf5ec92ccd116614
Federico Caballero <fedecaballero1234@gmail.com>
2025-05-07 09:35:05 -0300

    feat: creada ruta para alta logica de un producto

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 // Controladores
-const { getProducts, getProduct, createProduct, updateProduct, deleteProduct } = require("../controllers/productController");
+const { getProducts, getProduct, createProduct, updateProduct, deleteProduct, reactivateProduct } = require("../controllers/productController");
 const authenticate = require("../middlewares/authenticate");
 const { isAdmin } = require("../middlewares/isAdmin");
 
@@ -11,6 +11,7 @@ const { isAdmin } = require("../middlewares/isAdmin");
 router.post("/", authenticate, isAdmin, createProduct);
 router.put("/:id", authenticate, isAdmin, updateProduct);
 router.delete("/:id", authenticate, isAdmin, deleteProduct);
+router.post("/:id", authenticate, isAdmin, reactivateProduct);
 
 //No necesitan autenticacion
 router.get("/", getProducts);
