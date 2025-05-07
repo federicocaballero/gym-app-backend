48aaacf37da5f8633b3fc2acce374abee88e274a
Federico Caballero <fedecaballero1234@gmail.com>
2025-05-07 09:34:45 -0300

    feat: implementado controlador para alta logica de un producto

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,4 +1,4 @@
-const { crearProducto, mostrarProductosTodos, editarProducto, eliminarProducto, mostrarProductoPorId } = require("../models/Product");
+const { crearProducto, mostrarProductosTodos, editarProducto, eliminarProducto, mostrarProductoPorId, altaProducto } = require("../models/Product");
 
 exports.getProducts = async (req, res, next) => {
     try {
@@ -29,11 +29,11 @@ exports.createProduct = async (req, res, next) => {
         if (!nombre || nombre.trim() === '') {
             return res.status(400).json({ error: 'El nombre del producto es requerido' });
         }
-        
+
         if (precio_costo <= 0 || precio_venta <= 0) {
             return res.status(400).json({ error: 'Los precios deben ser mayores que cero' });
         }
-        
+
         if (precio_venta < precio_costo) {
             return res.status(400).json({ error: 'El precio de venta no puede ser menor al precio de costo' });
         }
@@ -58,21 +58,32 @@ exports.createProduct = async (req, res, next) => {
     }
 };
 
-exports.updateProduct = async (req, res, next) => {
+exports.deleteProduct = async (req, res, next) => {
     const { id } = req.params;
-    const { name, price, imagen } = req.body;
     try {
-        const response = await editarProducto(name, price, imagen, id);
+        const response = await eliminarProducto(id);
         res.status(200).json(response);
     } catch (error) {
         next(error);
     }
 }
 
-exports.deleteProduct = async (req, res, next) => {
+exports.reactivateProduct = async (req, res, next) => {
     const { id } = req.params;
     try {
-        const response = await eliminarProducto(id);
+        const response = await altaProducto(id);
+        res.status(200).json(response);
+    } catch (error) {
+        next(error);
+    }
+}
+
+exports.updateProduct = async (req, res, next) => {
+    //TODO: agregar validaciones y atributos que se pueden editar
+    const { id } = req.params;
+    const { name, price, imagen } = req.body;
+    try {
+        const response = await editarProducto(name, price, imagen, id);
         res.status(200).json(response);
     } catch (error) {
         next(error);
